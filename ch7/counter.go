9dc8c2933c1e03d3436c2048bc8eb9f512b046de
yang.wang <yang.wang@ushow.media>
2022-07-26 15:37:30 +0800

    提交代码

--- ch7/counter.go
+++ ch7/counter.go
@@ -16,3 +16,18 @@ func (c *WordCounter) Write(p []byte) (int, error) {
 	}
 	return int(*c), nil
 }
+
+type LineCounter int
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+	start := 0
+	for step := 0; start < len(p); start += step {
+		var err error
+		step, _, err = bufio.ScanLines(p[start:], true)
+		if err != nil {
+			return 0, err
+		}
+		*c += LineCounter(1)
+	}
+	return int(*c), nil
+}
--- ch7/counter_test.go
+++ ch7/counter_test.go
@@ -14,3 +14,13 @@ func TestWordCounter(t *testing.T) {
 	}
 	fmt.Printf("count=%d,word count=%d\v", n, w)
 }
+
+func TestLineCounter(t *testing.T) {
+	l := LineCounter(0)
+	n, err := fmt.Fprintf(&l, "this is a test by %s\n hello interface\n known something new", "owisho")
+	if err != nil {
+		t.Errorf("err=%v", err)
+		return
+	}
+	fmt.Printf("count=%d,line count=%d\v", n, l)
+}
